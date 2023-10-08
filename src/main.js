eaf24f16ebfc80ac084068aa9094e1f18b278bab
Fuxin Tang <2816129456@qq.com>
2023-10-08 22:58:41 +0800

    调整目录

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import '@/assets/main.scss'
 
 const app = createApp(App)
 
-// console.log(gaga)
-
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
